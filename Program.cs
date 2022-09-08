84df9153a896918b100fbeffcf32a9dc078d2972
mateus <mateusadc19@gmail.com>
2022-09-08 20:16:03 -0300

    Finalizar Tarefa

---
+++ Program.cs
@@ -0,0 +1,33 @@
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/dado/d{numeroDeFaces}", (
+    
+   [FromRoute] int numeroDeFaces
+    
+    ) => {
+    if (numeroDeFaces <= 0)
+    {   
+        return Results.BadRequest(new {mensagem = "Somente Dados com pelo menos uma Face"});
+    }
+
+    int face =  RandomNumberGenerator.GetInt32(1,numeroDeFaces + 1 );
+
+    return Results.Ok (new {dado = $"d{numeroDeFaces}" , rolagem = face });
+
+});
+
+app.Run();
